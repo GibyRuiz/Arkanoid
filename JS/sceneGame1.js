ed8da0210d95d8b661ec5eb7464508598549cea8
GibyRuiz <gibyruiz@gmail.com>
2020-08-21 00:29:29 -0300

    paleta y bola

--- JS/sceneGame1.js
+++ JS/sceneGame1.js
@@ -7,8 +7,12 @@ class SceneGame extends Phaser.Scene {
 
     preload(){
 
-        this.load.tilemapTiledJSON('tileArka', 'JSON/Arkanoid.json');
-        this.load.image('arka', 'Arkanoid.png');
+        this.load.tilemapTiledJSON('tileArka', 'JSON/Arkanoid.json')
+        this.load.image('arka', 'Arkanoid.png')
+        this.load.image('paleta', 'paleta.png')
+        this.load.image('bola', 'bola.png')
+
+
     }
 
     create(){
@@ -16,6 +20,9 @@ class SceneGame extends Phaser.Scene {
         this.map = this.make.tilemap({ key: 'tileArka' })
         this.tileset = this.map.addTilesetImage('Arkanoid', "arka")
         this.layer = this.map.createStaticLayer('Capa2', this.tileset, 13, 5)
+        this.paleta = this.add.sprite(400, 550, "paleta").setScale(.5)
+        this.bola = this.add.sprite(400, 480, "bola").setScale(.3)
 
+        
     }
 }
