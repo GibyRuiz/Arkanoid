8da8bd8d292260c66e5f85b69ed82da446268702
GibyRuiz <gibyruiz@gmail.com>
2020-08-22 12:14:02 -0300

    mejora rebote paleta

--- JS/sceneGame1.js
+++ JS/sceneGame1.js
@@ -19,26 +19,39 @@ class SceneGame extends Phaser.Scene {
 
         this.map = this.make.tilemap({ key: 'tileArka' })
         this.tileset = this.map.addTilesetImage('Arkanoid', "arka")
-        this.layer = this.map.createDynamicLayer('Capa2', this.tileset, 13, 5)
 
-        this.paleta = this.physics.add.sprite(400, 550, "paleta").setScale(.5).setCollideWorldBounds(true)
+        //Capa 2
+        // this.layer = this.map.createDynamicLayer('Capa2', this.tileset, 13, 5)
+
+        //Capa 1
+        this.layer = this.map.createDynamicLayer('Capa1', this.tileset, 13, 5)
+
+        this.paleta = this.physics.add.sprite(360, 550, "paleta").setScale(.5).setCollideWorldBounds(true).setOrigin(0,0)
         this.bola = this.physics.add.sprite(400, 530, "bola").setScale(.3).setCollideWorldBounds(true)
         this.bola.setBounce(1.1)
 
         this.cursors = this.input.keyboard.createCursorKeys()
 
+        this.bola.setVelocity(100,-300)
+
         this.physics.add.overlap(this.paleta, this.bola, () => {
 
-            if(this.bola.body.velocity.x < 0){
+            
 
-            this.bola.setVelocity(-100,-300)
+                if((this.bola.x - this.paleta.x) > 57){
 
-            }
+                    this.bola.setVelocity(100,-300)
 
-            else{
+                }
 
-                this.bola.setVelocity(100,-300)
-            }
+                else{
+
+                    this.bola.setVelocity(-100,-300)
+
+                }
+           
+                
+            
         })
 
         this.map.setCollision([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ]);
