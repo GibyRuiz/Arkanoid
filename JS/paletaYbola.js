e621df7f490bf6f40adb99d965fdb761541b5090
GibyRuiz <gibyruiz@gmail.com>
2020-08-30 10:57:07 -0300

    refactorización gameobjects

--- JS/paletaYbola.js
+++ JS/paletaYbola.js
@@ -8,6 +8,35 @@ class Paleta extends Phaser.Physics.Arcade.Sprite {
         this.setScale(.5)
         this.setCollideWorldBounds(true)
         this.setOrigin(0,0)
+
+        config.scene.cursors = config.scene.input.keyboard.createCursorKeys()
+        config.scene.spaceKey = config.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+        config.scene.update = () => {
+
+            if (config.scene.cursors.left.isDown)
+
+            {
+                this.setVelocity(-600, 0)
+            }
+
+            
+            else if (config.scene.cursors.right.isDown)
+
+            {
+                this.setVelocity(+600, 0)
+            }
+
+            else 
+            
+            {
+                this.setVelocity(0, 0)
+            }
+
+            if(config.scene.bola.y > 583){
+            
+                config.scene.bola.destroy()
+            }
+        }
     } 
 }
 
@@ -25,6 +54,14 @@ class Bola extends Phaser.Physics.Arcade.Sprite {
         this.setBounce(1.1)
         this.setVelocity(100, -300)
         this.paleta = this.escena.paleta
+        this.escena.map.setCollision([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ])
+
+        this.escena.physics.add.collider(this, this.escena.layer, (bola, tile) => {
+
+            this.escena.map.removeTile(tile)
+        })
+
+        this.escena.add.text(20, 580, 'Presiona "espacio" para cambiar de capa')
 
         this.escena.physics.add.overlap(this.paleta, this, () => {
             
--- JS/sceneGame1.js
+++ JS/sceneGame1.js
@@ -24,12 +24,6 @@ class SceneGame extends Phaser.Scene {
         this.paleta = new Paleta({scene: this, x: 360, y: 550, name: "paleta"})
         this.bola = new Bola({scene: this, x: 400, y: 530, name: "bola"})
 
-        this.cursors = this.input.keyboard.createCursorKeys()
-
-        this.text = this.add.text(20, 580, 'Presiona "espacio" para cambiar de capa')
-
-        this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
-
         this.spaceKey.on('up', function () {
             
             
@@ -37,41 +31,6 @@ class SceneGame extends Phaser.Scene {
         
         })
 
-
-        this.map.setCollision([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ])
-
-        this.physics.add.collider(this.bola, this.layer, (bola, tile) => {
-
-            this.map.removeTile(tile)
-        });
-
     }
 
-    update(){
-
-        if (this.cursors.left.isDown)
-
-            {
-                this.paleta.setVelocity(-600, 0)
-            }
-
-            
-        else if (this.cursors.right.isDown)
-
-            {
-                this.paleta.setVelocity(+600, 0)
-            }
-
-        else 
-            
-            {
-                this.paleta.setVelocity(0, 0)
-            }
-
-        if(this.bola.y > 583){
-            
-            this.bola.destroy()
-        }
-
-    }
 }
